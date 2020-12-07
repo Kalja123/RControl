41f0d770c13618451efc30e4f839031289e19c5f
Kalja123 <32325847+Kalja123@users.noreply.github.com>
2020-12-07 12:12:40 +0200

    Add files via upload

---
+++ RControl/examples/RControl_Bluetooth/RControl_Bluetooth.ino
@@ -0,0 +1,33 @@
+
+ #include <RControl.h>
+ 
+
+RControlBluetooth r1;
+
+
+void setup()
+{
+  // r1.pinA="{pin Nr(without a,d...);min value;max value;in/out;descripion}";
+  r1.pinA="{18;0;1023;in;humidity senser, value/10=dryness}{11;0;255;out;temp}";
+//r1.pinD="{pin Nr(without a,d...);in/out;descripion}";
+r1.pinD="{12;out;relay 1}{8;out;relay 2}{7;out;relay 3}";
+
+  pinMode(18, INPUT); 
+   pinMode(11, OUTPUT); 
+pinMode(12, OUTPUT);  
+  pinMode(8,OUTPUT);  
+ pinMode(7,OUTPUT);
+ 
+  Serial.begin(9600);
+  
+  r1.init(); //default values r1.init(Serial speed=9600);
+}
+
+
+void loop()
+{
+  
+  r1.listen();
+
+
+ }
